77270736edf5e8058c49ca5cbbb81a7973ee3b7d
bercovasile <bercovasile@gmail.com>
2023-11-18 23:44:07 +0200

    WEB_v0.5.1

--- web_html/app.py
+++ web_html/app.py
@@ -3,19 +3,30 @@
 from flask import Flask, request, send_file ,render_template ,redirect
 from flask import jsonify
 import os
-import  jpype     
-import  asposecells     
+import jpype     
+import asposecells     
+import logging
 jpype.startJVM() 
 from asposecells.api import Workbook
- 
-app = Flask(__name__ ,  template_folder='templates')
 
-# Directorul în care sunt stocate fișierele
-UPLOAD_FOLDER = '/home/bercovasile/Desktop/Programarea/Semestrul_3/proiect_de an/web_html'
+ 
+app = Flask(__name__ ,  template_folder='/templates')
+
+
+
+
+UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 
+#-----------------------------------------------------------
+
+
+
+
+#-----------------------------------------------------------
 
 @app.route('/')
 def index():
@@ -23,30 +34,57 @@ def index():
 
 @app.route('/student', methods=['GET'])
 def student():
-    print("Student")  # Afișați "student" în consolă
+    print("Student")  
     return send_file('templates/student.html')
-    #return jsonify(message="Student selected")  # Trimiteți un răspuns JSON către client
 
 @app.route('/teacher', methods=['GET'])
 def thacher():
-    print("Teacher")  # Afișați "student" în consolă
+    print("Teacher") 
     return send_file('templates/teacher.html')
-    #return jsonify(message="Student selected")  # Trimiteți un răspuns JSON către client
+
+#-----------------------------------------------------------
+
+
+
+
+#-----------------------------------------------------------
 
 @app.route('/get_options_teacher', methods=['GET'])
-def get_options_techer():
-    # Aici puteți genera sau obține opțiunile din backend
-    options_teacher = ["dsfsdfsd", "sdrser"] 
-    return jsonify(options=options_teacher)
-
-@app.route('/handle_selected_option', methods=['POST'])
-def handle_selected_option():
-    data = request.get_json()  # Obțineți datele JSON trimise de la client
-    selected_option = data.get("selectedOption")
-    print(f"Selected Option: {selected_option}")  # Afișați opțiunea aleasă în consolă
-    return jsonify(message=f"Selected Option: {selected_option}")
+def get_options_teacher():
+    options_teacher=[]
+    with open('./timetable/teacher/teacher.txt') as f:
+        for line in f:
+            options_teacher.append(line.strip())
+    return jsonify(options=options_teacher)  
 
 
+@app.route('/handle_selected_option_teacher', methods=['POST'])
+def handle_selected_option_teacher():
+    data = request.get_json()
+    selected_option_grups = data.get("selectedOptionTeacher")
+
+    print(f"Selected Option: {selected_option_grups}") 
+
+    excel_file_path = find_file_teacher(selected_option_grups)
+    print(f"Fint paht is {excel_file_path}")
+    return send_file(excel_file_path)
+
+def find_file_teacher(search_query):
+    excel_extensions = ['.xlsx', '.xls', '.xlsm', '.xlsb', '.xltx', '.xltm']  # Lista cu extensiile Excel
+
+    for root, dirs, files in os.walk("./timetable/teacher/"):
+        for file in files:
+            for ext in excel_extensions:
+                if file.endswith(ext) and search_query in file:
+                    return os.path.join(root, file)
+    return None
+
+#-----------------------------------------------------------
+
+
+
+
+#-----------------------------------------------------------
 
 @app.route('/get_options_grups', methods=['POST'])
 def get_options_grups():
@@ -54,22 +92,22 @@ def get_options_grups():
     selected_option = data.get("selectedOption")
     options_grups=[]
     if selected_option == "Anul 1" :
-        with open('templates/timetable/student/Anul 1/grupe.txt') as f:
+        with open('./timetable/student/Anul 1/grupe.txt') as f:
             for line in f:
                 options_grups.append(line.strip())
         return jsonify(options=options_grups)        
     elif selected_option == "Anul 2" :
-        with open('templates/timetable/student/Anul 2/grupe.txt') as f1:
+        with open('./timetable/student/Anul 2/grupe.txt') as f1:
             for line in f1:
                 options_grups.append(line.strip())
         return jsonify(options=options_grups)   
     elif selected_option == "Anul 3" :
-        with open('templates/timetable/student/Anul 3/grupe.txt') as f2:
+        with open('./timetable/student/Anul 3/grupe.txt') as f2:
             for line in f2:
                 options_grups.append(line.strip())
         return jsonify(options=options_grups) 
     elif selected_option == "Anul 4" :
-        with open('templates/timetable/student/Anul 4/grupe.txt') as f3:
+        with open('./timetable/student/Anul 4/grupe.txt') as f3:
             for line in f3:
                 options_grups.append(line.strip())
         return jsonify(options=options_grups)
@@ -80,34 +118,31 @@ def handle_selected_option_grups():
     selected_option_grups = data.get("selectedOptionGrups")
     print(f"Selected Option: {selected_option_grups}")  # Afișați opțiunea aleasă în consolă
     # Puteți face orice altă prelucrare aici
-    excel_file_path = '/home/bercovasile/Desktop/Programarea/Semestrul_3/proiect_de an/web_html/templates/timetable/student/Anul 1/TI-231.xlsx'
+    excel_file_path = find_file_student(selected_option_grups)
+    print(f"Fint paht is {excel_file_path}")
     # Returnați fișierul Excel către client
     return send_file(excel_file_path)
-    #workbook = Workbook("/home/bercovasile/Desktop/Programarea/Semestrul_3/proiect_de an/web_html/templates/timetable/student/Anul 1/orar_grup.xlsx")
-    
-    #jpype.shutdownJVM()
-    #return jsonify(workbook)
 
 
+def find_file_student(search_query):
+    excel_extensions = ['.xlsx', '.xls', '.xlsm', '.xlsb', '.xltx', '.xltm']  # Lista cu extensiile Excel
 
-
-
-
-
-def find_file(search_query):
-    for root, dirs, files in os.walk(app.config['UPLOAD_FOLDER']):
+    for root, dirs, files in os.walk("./timetable/student/"):
         for file in files:
-            if search_query in file:
-                return os.path.join(root, file)
+            for ext in excel_extensions:
+                if file.endswith(ext) and search_query in file:
+                    return os.path.join(root, file)
     return None
 
+#-----------------------------------------------------------
 
 
 
 
+#-----------------------------------------------------------
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5001,debug=True)
 
 
 
---
+++ web_html/static/js/student.js
@@ -0,0 +1,173 @@
+function updateSecondComboBox() {
+  const selectOptions = document.getElementById("selectOptions");
+  const selectedOption = selectOptions.value;
+
+  const selectOptionsGrups = document.getElementById("selectOptionsGrups");
+
+  fetch("/get_options_grups", {
+    method: "POST", // sau "GET" în funcție de necesități
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ selectedOption }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Ștergeți opțiunile actuale din al doilea combobox
+      selectOptionsGrups.innerHTML = "";
+
+      // Adăugați opțiunile noi în al doilea combobox
+      data.options.forEach(option => {
+        const optionElement = document.createElement("option");
+        optionElement.textContent = option;
+        optionElement.value = option;
+        selectOptionsGrups.appendChild(optionElement);
+      });
+    })
+    .catch(error => {
+      console.error("Error updating second combobox:", error);
+    });
+}
+
+function sendSelectedOptionGrups(test) {
+  const selectOptionsGrups = document.getElementById("selectOptionsGrups");
+  const selectedOptionGrups = selectOptionsGrups.value; // Obțineți valoarea opțiunii selectate
+  // Trimiteți opțiunea aleasă către backend folosind o cerere AJAX
+  fetch("/handle_selected_option_grups", {
+    method: "POST", // sau "GET" în funcție de necesități
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ selectedOptionGrups }),
+  })
+    .then(response => response.blob())
+    .then(blob => {
+
+      if (test)
+        excel_to_html_table_4(blob);
+      else
+        excel_to_html_table_5(blob);
+
+    })
+    .catch(error => {
+      console.error("Error sending selected option:", error);
+    });
+}
+
+
+function excel_to_html_table_4(excel_blob) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+    var data = new Uint8Array(event.target.result);
+    var workbook = XLSX.read(data, { type: "array" });
+    var sheet_name = workbook.SheetNames[0];
+    var sheet_data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name], { header: 1 });
+
+
+    if (sheet_data.length > 0) {
+      var table_output = '<table class="listViewTable table-sortable" id="listViewTable" >';
+      for (var row = 0; row < sheet_data.length; row = row + 2) {
+        var merge_data = '';
+        table_output += '<tr class="ce1 colorYellow">';
+
+        for (var cell = 0; cell < sheet_data[row].length; cell++) {
+          // Verificăm dacă valoarea este undefined și o înlocuim cu un spațiu gol
+          var cellValue = sheet_data[row][cell] === undefined ? " " : sheet_data[row][cell];
+
+          if (row == 0) {
+            table_output += '<th class="name1 hsText "><div class="table_first_row">' + cellValue + '</div></th>';
+          } else {
+
+            if (sheet_data[row + 1][cell] !== undefined) {
+
+              var undefinedCellValue = cellValue === '.' ? "📖" : cellValue;
+              table_output += '<td class="entry1 hsText"><div class="table_cell_content_merge_1"> ' + undefinedCellValue + '</div></td>';
+
+              var mergeCell = sheet_data[row + 1][cell] === '.' ? "📖" : sheet_data[row + 1][cell];
+              merge_data += '<td class="entry1 hsText"><div class="table_cell_content_merge_2">' + mergeCell + '</div></td>';
+
+            } else {
+              if ((cell == 0) && (row != 0)) {
+                var undefinedCellValue = cellValue === '.' ? "📖  <font siz</font>" : cellValue;
+                table_output += '<td class="entry1 hsText" rowspan="2"><div class="table_first_column">' + undefinedCellValue + '</div></td>';
+              }
+              else {
+                var undefinedCellValue = cellValue === '.' ? "📖" : cellValue;
+                table_output += '<td class="entry1 hsText" rowspan="2"><div class="table_cell_content">' + undefinedCellValue + '</div></td>';
+              }
+            }
+          }
+        }
+        table_output += '</tr>';
+        if (merge_data === '')
+          table_output += '<tr></tr>';
+        else
+          table_output += '<tr>' + merge_data + '</tr>';
+      }
+      table_output += '</table>';
+      document.getElementById('excel_data').innerHTML = table_output;
+    }
+  };
+
+  reader.readAsArrayBuffer(excel_blob);
+}
+
+
+function excel_to_html_table_5(excel_blob) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+    var data = new Uint8Array(event.target.result);
+    var workbook = XLSX.read(data, { type: "array" });
+    var sheet_name = workbook.SheetNames[0];
+    var sheet_data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name], { header: 1 });
+
+    if (sheet_data.length > 0) {
+      var table_output = '<table class="listViewTable table-sortable" id="listViewTable">';
+      for (var coll = 1; coll < sheet_data.length; coll++)
+        for (var row = 0; row < sheet_data.length; row = row + 2) {
+          var cellValue = sheet_data[row][coll];
+          var undefinedCellValue = cellValue === '.' ? "📖" : cellValue;
+
+          if (sheet_data[row][coll] !== undefined)
+            if (sheet_data[row + 1][coll] !== undefined) {
+
+              table_output += '<tr class="ce1 colorYellow"> <td class="entry1 hsText"><div class="table_cell_content_merge_1">' + undefinedCellValue + '</div></td> </tr>';
+
+              var cellValue = sheet_data[row + 1][coll];
+              var undefinedCellValue = cellValue === '.' ? "📖" : cellValue;
+              table_output += '<tr class="ce1 colorYellow"> <td class="entry1 hsText"><div class="table_cell_content_merge_2">' + undefinedCellValue + '</div></td> </tr>';
+            }
+            else
+              if (row == 0)
+                table_output += '<tr class="ce1 colorYellow"> <td class="entry1 hsText"><div class="table_first_row">' + undefinedCellValue + '</div></td> </tr>';
+              else
+                table_output += '<tr class="ce1 colorYellow"> <td class="entry1 hsText"><div class="table_cell_content">' + undefinedCellValue + '</div></td> </tr>';
+        }
+
+
+      table_output += '</table>';
+      document.getElementById('excel_data').innerHTML = table_output;
+    }
+  };
+  reader.readAsArrayBuffer(excel_blob);
+}
+
+function checkScreenWidth() {
+  const width = window.innerWidth;
+
+  if (width > 600) {
+    sendSelectedOptionGrups(1);
+  } else {
+    sendSelectedOptionGrups(0);
+  }
+}
+
+// Apelăm funcția inițial pentru a seta valoarea corectă la încărcarea paginii
+checkScreenWidth();
+
+// Adăugăm un ascultător pentru evenimentul resize al ferestrei pentru a verifica și apela funcția corespunzătoare
+window.addEventListener('resize', checkScreenWidth);
+
+
+
+
